bc68a0d621dd74bf191291adeeaf5e3d362da478
AmanKumar-99 <49776680+AmanKumar-99@users.noreply.github.com>
2020-03-28 09:26:59 +0530

    Node class created...

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -1,10 +1,38 @@
 /*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:13 PM
+ * 
+ *  User: Aman
+ *  Date: 26-Mar-20
+ *  Time: 8:29 PM
  */
 package problem3.node;
+import problem5.student.Student;
 
 public class Node {
+
+private Student s;
+    private  Node next;
+
+
+    public Student getS() {
+        return s;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node()
+    {
+        s=new Student();
+        next=null;
+    }
+
+    public void setS(Student s) {
+        this.s = s;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
 }
